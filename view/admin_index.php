fa805f96a079f5879b77b3fa30d8a2832a8133eb
ivakozjak <ivkozja@student.math.hr>
2021-07-11 19:01:18 +0200

    kartice admin novi kviz/pitanje

--- view/admin_index.php
+++ view/admin_index.php
@@ -1,10 +1,32 @@
 <?php require_once __DIR__ . '/header_admin.php'; ?>
-
-<form class="forma" method="post" action="admin.php?rt=admin/createQuiz">
-    <label><button class="dodaj" type="submit">Kreiraj novi kviz</button></label>
-</form>
-<form class="forma" method="post" action="admin.php?rt=admin/addQandA">
-    <label><button class="dodaj" type="submit">Dodaj nova pitanja u postojeći kviz</button></label>
-</form>
+<script src="JS/flip.js"></script>
+<div class="grid-container">
+<div class="maincontainer">
+  <div class="kartica" onclick="flip(event)">
+    <div class="kartica-front">
+        <img src="app/novi_kviz.jpg" width="240" height="80" class="image_quiz">
+    </div>
+    <div class="kartica-back">
+        <br><br><br><br><br><br>
+        <form class="forma" method="post" action="admin.php?rt=admin/createQuiz">
+        <label><button class="dodaj" type="submit">Kreiraj novi kviz</button></label>
+        </form>
+    </div>
+  </div>
+</div>
+<div class="maincontainer">
+  <div class="kartica" onclick="flip(event)">
+    <div class="kartica-front">
+        <img src="app/novo_pitanje.jpg" width="240" height="80" class="image_quiz">
+    </div>
+    <div class="kartica-back">
+        <br><br><br><br><br><br>
+        <form class="forma" method="post" action="admin.php?rt=admin/addQandA">
+        <label><button class="dodaj" type="submit">Dodaj nova pitanja u postojeći kviz</button></label>
+        </form>
+    </div>
+  </div>
+</div>
+</div>
 
 <?php require_once __DIR__ . '/_footer.php'; ?>
