ebeea20152a5756fd7a483b5cf988e95e856441e
ivakozjak <ivkozja@student.math.hr>
2021-07-11 14:04:57 +0200

    prikaz kvizova kartice

--- view/quizzes_index.php
+++ view/quizzes_index.php
@@ -1,10 +1,28 @@
 <?php require_once __DIR__ . '/_header.php';
-
+?>
+<div class="grid-container">
+<?php
 foreach ($quizList as $quiz) {
     $name = strtolower($quiz->name); //tako je spremljeno u mapi app
     $path = dirname($_SERVER['PHP_SELF']);
-    echo '<img src="' . $path . '/app/' . $name . '.jpg" width="240" height="80" class="image_quiz">' .
-        '<p>' . $quiz->name . '</p>';
+    ?>
+    <div class="maincontainer">
+      <div class="kartica">
+        <div class="kartica-front">
+    <?php
+        echo '<img src="' . $path . '/app/' . $name . '.jpg" width="240" height="80" class="image_quiz">' . '<p>' . $quiz->name . '</p>';
+    ?>
+        </div>
+        <div class="kartica-back">Tipovi pitanja:</div>
+      </div>
+    </div>
+    <?php
 }
+?>
+</div>
+</body>
+
+</html>
+<?php
 
 require_once __DIR__ . '/_footer.php';
