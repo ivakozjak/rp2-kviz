f65879a783b64f58dd9fccf584246f27d2a74fbd
igormaricpmf <igor.maric@student.math.hr>
2021-07-12 10:24:53 +0200

    Update create_question.php
    forma za popuni tip

--- view/create_question.php
+++ view/create_question.php
@@ -34,6 +34,17 @@ require_once __DIR__ . '/header_admin.php';
                     'tip1"' + '><label>Odgovor 1:<p id="odgovor1">T</p></label><label>Odgovor 2:<p id="odgovor2">N</p></label><label><button class="ulogirajse"' + 'type = "submit"' + 'name="submit"' + 'value="Dodaj"' +
                     'id="btn_dodaj">Dodaj</button></label></div>');
             }
+            else if (question_type === "id_type3") {
+                $("#pitanja").remove();
+                $("body").append('<div id="' +
+                    'tip2"' + '><label>Tekst pitanja <input type="text" id="pitanje" name="pitanje"></label>'
+                    +'<label>Odgovor1 <input type="text" id="answer1" name="answer"></label>'+
+                    '<label>Odgovor2 <input type="text" id="answer2" name="answer"></label>'
+                    +'<label>Odgovor3 <input type="text" id="answer3" name="answer"></label>'
+                    +'<label>Odgovor4 <input type="text" id="answer4" name="answer"></label>'+
+                    '<label><button class="ulogirajse"' + 'type = "submit"' + 'name="submit"' + 'value="Dodaj"' +
+                    'id="btn_dodaj">Dodaj</button></label></div>');
+            }
         } else {
             alert("Potrebno je ispuniti sva polja!");
         }
